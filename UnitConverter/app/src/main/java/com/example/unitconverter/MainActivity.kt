ad2c2705567222a61edf46537952d4b576bffbf3
GunWoo <kkw31025@gmail.com>
2024-06-04 14:59:18 +0900

    Android&Kotlin Ch6

--- UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.unitconverter
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -10,7 +9,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
@@ -24,12 +22,17 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,8 +41,7 @@ class MainActivity : ComponentActivity() {
             UnitConverterTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     UnitConverter()
                 }
@@ -50,7 +52,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter() {
-
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var inputUnit by remember { mutableStateOf("Select") }
+    var outputUnit by remember { mutableStateOf("Select") }
+    var iconverterRate by remember { mutableDoubleStateOf(1.00) }
+    var oconverterRate by remember { mutableDoubleStateOf(1.00) }
+    var dropDownState1 by remember { mutableStateOf(false) }
+    var dropDownState2 by remember { mutableStateOf(false) }
+    fun convertunits() {
+        val inputValueDouble = inputValue.toDoubleOrNull() ?: 0.0
+        val result =
+            (inputValueDouble * iconverterRate * 100.0 / oconverterRate).roundToInt() / 100.0
+        outputValue = result.toString()
+    }
+    0
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -59,37 +75,88 @@ fun UnitConverter() {
         //ui 요소들을 하나씩 추가할 부분
         Text(text = "Unit Converter")
         Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = "", onValueChange = {})
+        OutlinedTextField(value = inputValue,
+            onValueChange = { inputValue = it },
+            label = { Text(text = "value를 입력하세요") })
         Spacer(modifier = Modifier.height(16.dp))
         Row {
             Box {
-                Button(onClick = { }) {
-                    Text(text = "Select")
+                Button(onClick = { dropDownState1 = true }) {
+                    Text(text = inputUnit)
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
                 }
-                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
-                    DropdownMenuItem(text = { Text(text = "Centimeters") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text(text = "Meters") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text(text = "Feet") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text(text = "Milimeters") }, onClick = { /*TODO*/ })
+                DropdownMenu(expanded = dropDownState1,
+                    onDismissRequest = { dropDownState1 = false }) {
+                    DropdownMenuItem(text = { Text(text = "Centimeters") }, onClick = {
+                        dropDownState1 = false;
+                        inputUnit = "Centimeters";
+                        iconverterRate = 0.01
+                        convertunits()
+                    })
+                    DropdownMenuItem(text = { Text(text = "Meters") }, onClick = {
+                        dropDownState1 = false;
+
+                        inputUnit = "Centimeters";
+                        iconverterRate = 1.0
+                        convertunits()
+                    })
+                    DropdownMenuItem(text = { Text(text = "Feet") }, onClick = {
+                        dropDownState1 = false;
+
+                        inputUnit = "Centimeters";
+                        iconverterRate = 0.3048
+                        convertunits()
+                    })
+                    DropdownMenuItem(text = { Text(text = "Milimeters") }, onClick = {
+                        dropDownState1 = false;
+
+                        inputUnit = "Centimeters";
+                        iconverterRate = 0.001
+                        convertunits()
+                    })
                 }
             }
             Spacer(modifier = Modifier.width(16.dp))
             Box {
-                Button(onClick = {} ) {
-                    Text(text = "Select")
+                Button(onClick = { dropDownState2 = true }) {
+                    Text(text = outputUnit)
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
                 }
-                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
-                    DropdownMenuItem(text = { Text(text = "Centimeters") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text(text = "Meters") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text(text = "Feet") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text(text = "Milimeters") }, onClick = { /*TODO*/ })
+                DropdownMenu(expanded = dropDownState2,
+                    onDismissRequest = { dropDownState2 = false }) {
+                    DropdownMenuItem(text = { Text(text = "Centimeters") }, onClick = {
+                        dropDownState2 = false;
+
+                        outputUnit = "Centimeters";
+                        oconverterRate = 0.01
+                        convertunits()
+                    })
+                    DropdownMenuItem(text = { Text(text = "Meters") }, onClick = {
+                        dropDownState2 = false;
+
+                        outputUnit = "Meters";
+                        oconverterRate = 1.0
+                        convertunits()
+                    })
+                    DropdownMenuItem(text = { Text(text = "Feet") }, onClick = {
+                        dropDownState2 = false;
+
+                        outputUnit = "Feet";
+
+                        oconverterRate = 0.3048
+                        convertunits()
+                    })
+                    DropdownMenuItem(text = { Text(text = "Milimeters") }, onClick = {
+                        dropDownState2 = false;
+                        outputUnit = "Milimeters";
+                        oconverterRate = 0.001
+                        convertunits()
+                    })
                 }
             }
         }
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Result:")
+        Text(text = "Result:${outputValue}")
     }
 }
 
